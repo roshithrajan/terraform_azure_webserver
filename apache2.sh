c4a698222d84e8ae0a4f7d6b0c4bbbeb3c29e6f9
roshithrajan <roshi1983@gmail.com>
2023-01-20 12:31:15 +0530

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -3,4 +3,4 @@
 sudo apt-get update && sudo apt -y install apache2
 
 
-echo '<!doctype html><html><body><h1>Hello Roshith if you see this than you have apache running!</h1></body></html>' | sudo tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>Hello Roshith rajan if you see this than you have apache running!</h1></body></html>' | sudo tee /var/www/html/index.html
